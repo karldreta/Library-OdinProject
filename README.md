2cff4b54ec638a15f9821caaa197d77e130328c2
karldreta <karldreta@gmail.com>
2024-06-15 10:23:04 +0800

    Add book form modal

--- README.md
+++ README.md
@@ -12,3 +12,8 @@ Project: Library from JS path of The Odin Project
     * Prototypal Inheritance
 
 * For the moment this project is only a an early draft. I am not yet expected to add any type of storage to save the information, since "I will have the option to come back to this project later on in the course".
+
+
+## Reference:
+
+[Web Dev Simplified: Dialog](https://blog.webdevsimplified.com/2023-04/html-dialog/)
--- script.js
+++ script.js
@@ -4,6 +4,13 @@ const books = document.querySelectorAll('.book');
 books.forEach(book => book.addEventListener('click', displayBook));
 const button = document.querySelector('#addBook');
 button.addEventListener('click', addBookToLib);
+const addBookForm = document.querySelector('.book-form-container');
+const modals = document.querySelectorAll('dialog');
+const closeModalButtons = document.querySelectorAll('.closeModal');
+closeModalButtons.forEach(button => button.addEventListener('click', () => {
+    document.querySelector('.modal').close();
+}));
+
 
 const myLibrary = [];
 
@@ -14,13 +21,30 @@ function Book(title, author, pages, isRead) {
     this.isRead = isRead;
     this.info = function() { return `${this.title} by ${this.author} has ${this.pages} pages, ${this.isRead}.`;
     }
+    // Add a finished reading date()
+
     myLibrary.push(this);
 }
 
 function displayBook() {
-
+    // This will be a function for displaying the info of a book in a Modal.
 }
 
 function addBookToLib() {
-
+    addBookForm.showModal();
 }
+
+
+modals.forEach(modal => {
+    modal.addEventListener("click", e => {
+      const dialogDimensions = modal.getBoundingClientRect();
+      if (
+        e.clientX < dialogDimensions.left ||
+        e.clientX > dialogDimensions.right ||
+        e.clientY < dialogDimensions.top ||
+        e.clientY > dialogDimensions.bottom
+      ) {
+        modal.close();
+      }
+    });
+  });
